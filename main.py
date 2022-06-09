7418c5de202f4d29869fa6065e3fa5dd1b2fe5ab
Maxim <maxim.geer@yandex.ru>
2022-06-09 20:14:43 +0500

    added move check

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from asyncio.windows_events import NULL
+from tkinter import X
 from turtle import back, pos
 from xmlrpc.client import Boolean
 import pygame
@@ -63,6 +64,57 @@ for i in range(0, len(board)):
             figure.rank = -2
         all_sprites.add(figure)
 
+def remove_figure_with_pos(pos):
+    for figure in all_sprites:
+        figure_pos = get_square_number((figure.rect.x, figure.rect.y))
+        if figure_pos[0] == pos[0] and figure_pos[1] == pos[1] and figure.rank != -2:
+            all_sprites.remove(figure)
+            board[pos[1] * BOARD_WH + pos[0]] = 0
+            return 0
+    return -1
+
+def control_figures(figure):
+    figure_pos = get_square_number((figure.rect.x, figure.rect.y))
+    if figure.rank == -2:
+        if (figure_pos[0], figure_pos[1]) == (0, 0) or (figure_pos[0], figure_pos[1]) == (BOARD_WH - 1, BOARD_WH - 1) or \
+            (figure_pos[0], figure_pos[1]) == (0, BOARD_WH - 1) or (figure_pos[0], figure_pos[1]) == (BOARD_WH - 1, 0):
+            return 1 #attackers lose
+    else:
+        for index in range(0, BOARD_POS):
+            if board[index] == -2:
+                king_x = index % BOARD_WH
+                king_y = index // BOARD_WH
+                if king_x > 0 and king_y > 0 and king_x < BOARD_WH - 1 and king_y < BOARD_WH - 1:
+                    if board[(king_y - 1) * BOARD_WH + king_x] == 1 and board[(king_y + 1) * BOARD_WH + king_x] == 1 \
+                        and board[king_y * BOARD_WH + (king_x - 1)] == 1 and board[king_y * BOARD_WH + (king_x + 1)] == 1:
+                        return 2 #attackers win
+    if figure.rank < 0:
+        if figure_pos[0] > 1 and board[figure_pos[1] * BOARD_WH + (figure_pos[0] - 2)] < 0 \
+            and board[figure_pos[1] * BOARD_WH + (figure_pos[0] - 1)] > 0:
+            remove_figure_with_pos((figure_pos[0] - 1, figure_pos[1]))
+        if figure_pos[1] > 1 and board[(figure_pos[1] - 2) * BOARD_WH + figure_pos[0]] < 0 \
+            and board[(figure_pos[1] - 1) * BOARD_WH + figure_pos[0]] > 0:
+            remove_figure_with_pos((figure_pos[0], figure_pos[1] - 1))
+        if figure_pos[0] < BOARD_WH - 2 and board[figure_pos[1] * BOARD_WH + (figure_pos[0] + 2)] < 0 \
+            and board[figure_pos[1] * BOARD_WH + (figure_pos[0] + 1)] > 0:
+            remove_figure_with_pos((figure_pos[0] + 1, figure_pos[1]))
+        if figure_pos[1] < BOARD_WH - 2 and board[(figure_pos[1] + 2) * BOARD_WH + figure_pos[0]] < 0 \
+            and board[(figure_pos[1] + 1) * BOARD_WH + figure_pos[0]] > 0:
+            remove_figure_with_pos((figure_pos[0], figure_pos[1] + 1))
+    else:
+        if figure_pos[0] > 1 and board[figure_pos[1] * BOARD_WH + (figure_pos[0] - 2)] > 0 \
+            and board[figure_pos[1] * BOARD_WH + (figure_pos[0] - 1)] < 0:
+            remove_figure_with_pos((figure_pos[0] - 1, figure_pos[1]))
+        if figure_pos[1] > 1 and board[(figure_pos[1] - 2) * BOARD_WH + figure_pos[0]] > 0 \
+            and board[(figure_pos[1] - 1) * BOARD_WH + figure_pos[0]] < 0:
+            remove_figure_with_pos((figure_pos[0], figure_pos[1] - 1))
+        if figure_pos[0] < BOARD_WH - 2 and board[figure_pos[1] * BOARD_WH + (figure_pos[0] + 2)] > 0 \
+            and board[figure_pos[1] * BOARD_WH + (figure_pos[0] + 1)] < 0:
+            remove_figure_with_pos((figure_pos[0] + 1, figure_pos[1]))
+        if figure_pos[1] < BOARD_WH - 2 and board[(figure_pos[1] + 2) * BOARD_WH + figure_pos[0]] > 0 \
+            and board[(figure_pos[1] + 1) * BOARD_WH + figure_pos[0]] < 0:
+            remove_figure_with_pos((figure_pos[0], figure_pos[1] + 1))
+    return 0
 
 def get_square_number(pos):
     x = int((pos[0] - BOARD_POS) / SQUARE_WH)
@@ -77,6 +129,12 @@ def move_figure(mouse_pos, figure):
     square_pos = get_square_number(mouse_pos)
     square_with_figure_pos = get_square_number(figure_pos)
     if square_pos == None or square_with_figure_pos == None:
+        return (x, y)
+    elif (square_pos[0] == 0 and square_pos[1] == 0) or (square_pos[0] == BOARD_WH - 1 and square_pos[1] == BOARD_WH - 1) \
+    or (square_pos[0] == 0 and square_pos[1] == BOARD_WH - 1) or (square_pos[0] == BOARD_WH - 1 and square_pos[1] == 0):
+        if figure.rank != -2:
+            return (x, y)
+    elif square_pos[0] == BOARD_WH // 2 and square_pos[1] == BOARD_WH // 2:
         return (x, y)
     if square_pos[0] == square_with_figure_pos[0] or square_pos[1] == square_with_figure_pos[1]:
         flag = True
@@ -113,7 +171,6 @@ while running: #main loop
             for sprite in all_sprites:
                 if sprite.rect.collidepoint(e.pos):
                     selected_figure = sprite
-                    print(selected_figure)
         elif e.type == pygame.MOUSEBUTTONUP and selected_figure is not None:
             for sprite in all_sprites:
                 if sprite == selected_figure:
@@ -128,6 +185,7 @@ while running: #main loop
                         sprite.rect.y = position[1]
                         selected_figure = None
                         isAttackersMove = not isAttackersMove
+                        print(control_figures(sprite))
                         break
     all_sprites.draw(screen)
     pygame.display.flip() #двойная буферезация
